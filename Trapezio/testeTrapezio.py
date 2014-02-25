92848acabd338aa590b9e0ef53f8cc50038a1c3b
PCarllay <pcarllay@gmail.com>
2014-02-25 18:47:57 -0300

    Regra do trapezio
    ainda falta alterar para ler os dados do arquivo

---
+++ Trapezio/testeTrapezio.py
@@ -0,0 +1,25 @@
+import leitura1
+
+print leitura1.valor_m
+
+m=leitura1.valor_m
+print leitura1.ler()
+x0=1.0
+xn=3.0
+
+fxo=0.0
+fx1=1.3684
+fx2=5.5452
+fx3=14.317
+fx4=29.6625
+
+h=(xn-x0)/m
+print h
+s = 2*(fx1+fx2+fx3)
+aux = (h/2)
+print s
+print aux
+It=aux*(fxo + s+fx4)
+
+print It
+
