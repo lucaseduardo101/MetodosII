4076dd70e9c7eb363e7aebc5ca153f9ebd760567
lucaseduardo101 <lucaseduardo101@gmail.com>
2014-03-01 00:53:54 -0300

    .

--- Simpsons38/simpson38.py
+++ Simpsons38/simpson38.py
@@ -1,16 +1,23 @@
 import leitura
+import sys
+arq = sys.argv[1]
 
-l = leitura.ler()
+l = leitura.ler(arq)
+x = []
+y = []
 m = l[0]
+somatorio = 0.0
+simpson = 0.0
 
 h = (l[m+1][0] - l[1][0]) / m
 print h
 
-x = [0.0, 0.5, 1.0,   1.5]
-y = [0.0, 0.35, 0.55, 0.9]
-
-somatorio = 0.0
-simpson = 0.0	
+for i in range ( 1,m +2):
+	x.append( l[i][0] )
+	y.append( l[i][1] )
+	
+print x
+print y	
 
 for i in range(0,len(y)):
 	if ( ( i == 0 ) or ( i == len(x) - 1 ) ): 
